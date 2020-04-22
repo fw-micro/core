fea4c4ede2b47004d4eba37e239775a067edaff1
o.bielokudrenko <oleksii.bielokudrenko@pm.bet>
2020-04-22 12:58:24 +0300

    update controller

--- controller/BaseController.php
+++ controller/BaseController.php
@@ -3,6 +3,7 @@
 namespace fw_micro\core\controller;
 
 use fw_micro\core\response\Response;
+use fw_micro\pattern\Register\Register;
 
 /**
  * Class BaseController
@@ -11,6 +12,53 @@ use fw_micro\core\response\Response;
 class BaseController
 {
     protected $layout;
+
+    public function __construct()
+    {
+        $accessList = $this->accessList ?? ['*' => ['*']];
+        if (!$this->checkAccess($accessList)) {
+            $response = new Response();
+            $response->setHeader('HTTP/1.1 403 Forbidden');
+            $response->print();
+            die;
+        }
+        $this->init();
+    }
+
+    public function init()
+    {
+    }
+
+    public function getUserGroup()
+    {
+        return '*';
+    }
+
+    public function checkAccess(array $accessList)
+    {
+        if (in_array('*', $accessList['*'] ?? [])) {
+            return true;
+        }
+
+        $group = $this->getUserGroup();
+        if (!is_array($accessList[$group])) {
+            return false;
+        }
+
+        $list = $accessList[$group];
+        $default = $list['default'] ?? null;
+        $action = Register::get()->request['action'];
+
+        if (in_array($action, $list)) {
+            return true;
+        }
+
+        if ($default) {
+            return $this->redirect($default);
+        }
+
+        return false;
+    }
 
     /**
      * @param string $file
@@ -33,5 +81,17 @@ class BaseController
         }
         $response->setBody($html);
         return $response;
+    }
+
+    public function redirect(string $url): Response
+    {
+        return (new Response())->setHeader('Location: ' . $url);
+    }
+
+    public function renderJson(array $data): Response
+    {
+        return (new Response())
+            ->setBody(json_encode($data))
+            ->setHeader('Content-Type: application/json');
     }
 }
